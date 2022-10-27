9e4f7e013ed145843f8698092f7332025cef42a0
Tech I.S <techi.s.@Techs-MacBook-Air-86.local>
2022-10-27 15:39:52 +0530

    frontend deployment

---
+++ src/components/Common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ImgLogo from "../../assets/img/logo.svg";
+
+const Footer = () => {
+  return (
+    <footer>
+     
+    </footer>
+  );
+};
+
+export default Footer;
